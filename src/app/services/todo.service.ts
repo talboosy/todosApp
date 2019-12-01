109bdc8f0a76cabf554ce50e223d16bd4244f9f2
Tal Boosy <tal@allegro.ai>
2019-12-01 20:30:07 +0200

    added routing, finished add todo

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -36,4 +36,9 @@ export class TodoService {
     const url =`${this.todosUrl}/${todo.id}`
     return this.http.put(url, todo, httpOptions)
   }
+
+  //Add Todo
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
 }
