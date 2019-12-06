3983581d5fce25a4dac5ba7d3193eac89f087b9f
Tal Boosy <tal@allegro.ai>
2019-12-06 14:23:47 +0200

    firebase initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
 import { AngularFirestore } from "angularfire2/firestore";
-
+import { FIREBASE_KEYS } from "./keys"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,16 +17,7 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { AboutComponent } from './components/pages/about/about.component';
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyC899GNXZRIz7AexD7y-nzj5N4dwseo00U",
-  authDomain: "boostodo-84224.firebaseapp.com",
-  databaseURL: "https://boostodo-84224.firebaseio.com",
-  projectId: "boostodo-84224",
-  storageBucket: "boostodo-84224.appspot.com",
-  messagingSenderId: "100608680039",
-  appId: "1:100608680039:web:a3687d7c318e2993e574c2",
-  measurementId: "G-HGB9RFG2J3"
-}
+export const firebaseConfig = FIREBASE_KEYS;
 
 @NgModule({
   declarations: [
